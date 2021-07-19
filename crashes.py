f524bd194d1005d00345c3ad3d61523408190a7d
Harsha Somisetty <harshasomisetty7@gmail.com>
2021-07-19 19:26:23 -0400

    finished market crashes paper fully

--- crashes.py
+++
@@ -1,9 +0,0 @@
-from data import *
-import numpy as np
-
-# def get_point_clouds
-
-if __name__ == "__main__":
-    data = load_data("data/basic_data.csv")
-    # print(data["SPY.Open"])
-    print(data.head())
